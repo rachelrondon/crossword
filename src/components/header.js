3e45f9e4de8685484f2df0cf963a498632cb7e6f
Rachel Rondon <rachel.sun.rondon@gmail.com>
2018-12-16 12:10:15 -0500

    layout updates

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,10 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: 'white',
       marginBottom: '1.45rem',
+      boxShadow: 'rgba(0, 0, 0, 0.05) 0px 10px 20px, rgba(0, 0, 0, 0.2) 0px 0px 1px',
+      textAlign: 'center',
     }}
   >
     <div
@@ -20,7 +22,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: 'white',
+            color: 'black',
             textDecoration: 'none',
           }}
         >
